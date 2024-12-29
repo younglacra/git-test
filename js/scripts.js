f0a0806ad86fe2eea28b7929b044b6feef75a7f8
Alberto <albertoblancosole@hotmail.com>
2024-12-29 13:50:32 -0600

    Initial commit for NuCamp project

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const carousel = new bootstrap.Carousel('#homeCarousel', {
+    interval: 5000,
+    pause: false
+})
+
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+}) 
